aa6549184df4233cc7bafda5fdcf88c0ca6ffecb
Kodon <amirultgr@gmail.com>
2026-03-29 05:29:02 +0700

    Login, Regist, Profil Manajemen, Retail Manajemen, Dashboard selesai develop

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Inter',
         useMaterial3: true,
       ),
-      home: const AuthGate(), // AuthGate akan menentukan halaman berdasarkan status login
+      home: const AuthGate(),
     );
   }
 }
