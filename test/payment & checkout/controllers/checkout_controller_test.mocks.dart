249f0c242f1b71f25dd8701bf51d9a6cc4ec5344
Andhika Pratama Adhi Surya <andhikapratama1510@gmail.com>
2026-05-23 19:48:13 +0700

    save

--- test/payment & checkout/controllers/checkout_controller_test.mocks.dart
+++ test/payment & checkout/controllers/checkout_controller_test.mocks.dart
@@ -1209,7 +1209,6 @@ class MockPushNotificationService extends _i1.Mock
 /// A class which mocks [CollectionReference].
 ///
 /// See the documentation for Mockito's code generation for more information.
-// ignore: must_be_immutable
 class MockCollectionReference<T extends Object?> extends _i1.Mock
     implements _i4.CollectionReference<T> {
   MockCollectionReference() {
@@ -1621,7 +1620,6 @@ class MockCollectionReference<T extends Object?> extends _i1.Mock
 /// A class which mocks [DocumentReference].
 ///
 /// See the documentation for Mockito's code generation for more information.
-// ignore: must_be_immutable
 class MockDocumentReference<T extends Object?> extends _i1.Mock
     implements _i4.DocumentReference<T> {
   MockDocumentReference() {
